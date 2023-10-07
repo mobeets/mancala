08013548b109c58879f5ba2570bc8894153fcf57
Jay H <mobeets@gmail.com>
2023-10-07 16:46:38 -0400

    play as cpu or human

--- play.py
+++ play.py
@@ -1,73 +1,6 @@
 from mancala import Mancala
 import numpy as np
 
-def estimate_value(data, player_num):
-    # data is list of (state, action, next_state)
-    # we only use current state to figure out whose turn it is
-    # and also check if game is over, in which case we need no heuristic
-    state = data[-1][2]
-    if Mancala.is_terminated(state):
-        # no heuristic needed: value is inf if we won, -inf if we lost
-        return np.inf if Mancala.get_winner(state) == player_num else -np.inf
-    else:
-        start_state = data[0][0]
-        s1_init, s2_init = Mancala.get_current_scores(start_state)
-        s1_final, s2_final = Mancala.get_current_scores(state)
-        # value is equal to our improvement in score differential
-        score_delta = (s1_final - s2_final) - (s1_init - s2_init)
-        return score_delta if start_state[-1] == 1 else -score_delta
-
-def get_auto_action(state):
-    # if any action leads to an extra move, we make them here
-    actions = Mancala.get_valid_actions(state)
-    possible_auto_actions = []
-    for action in actions:
-        next_state = Mancala.next_state(state, action)
-        if next_state[-1] == state[-1]:
-            # taking this action would not change whose turn it is
-            possible_auto_actions.append(action)
-    if possible_auto_actions:
-        # take the auto action closest to our mancala first
-        # to ensure we can take the others on our next turn
-        return max(possible_auto_actions)
-    else:
-        return None
-
-def rollout_best_move(state, action, depth, nsamples):
-    data = []
-    for d in range(depth+1):
-        if d > 0:
-            action = best_move(state, depth-d, nsamples)        
-        next_state = Mancala.next_state(state, action)
-        data.append((state, action, next_state))
-        state = next_state
-    return data
-
-def best_move(state, depth, nsamples=1):
-    # first check for any automatic moves, and add them here
-    chosen_action = get_auto_action(state)
-    if chosen_action:
-        return chosen_action
-
-    # now check for best next move
-    actions = Mancala.get_valid_actions(state)
-    values = []
-    for action in actions:
-        cvalues = []
-        for _ in range(nsamples):
-            data = rollout_best_move(state, action, depth, nsamples)
-            value = estimate_value(data, state[-1])
-            cvalues.append(value)
-        values.append(np.mean(cvalues))
-    return actions[np.argmax(values)]
-
-class HeuristicAgent:
-    def __init__(self, depth=2):
-        self.depth = depth
-
-    def get_action(self, state):
-        return best_move(state, self.depth)
-
 # def sim(opening_actions, depth, check_auto_first=False, do_render=False):
 #     state = initial_game_state()
 #     if do_render:
@@ -102,11 +35,13 @@ class HeuristicAgent:
 #             score_diff = 'DNE'
 #         print(first_action, score_diff, nsteps)
 
-# MCTS
+# Pure Monte Carlo Rollout
 
-class MCTSAgent:
-    def __init__(self, nsamples=1000):
+class MonteCarloRolloutAgent:
+    def __init__(self, name=None, nsamples=1000, verbose=False):
+        self.name = name
         self.nsamples = nsamples
+        self.verbose = verbose
 
     def get_rollout_policy_action(self, state):
         actions = Mancala.get_valid_actions(state)
@@ -140,11 +75,12 @@ class MCTSAgent:
                 payout = self.get_return(data, state[-1])
                 payouts.append(payout)
             mean_payouts.append(np.mean(payouts))
-        return actions[np.argmax(mean_payouts)], np.max(mean_payouts)
+        return actions[np.argmax(mean_payouts)], np.array(mean_payouts)
 
     def get_action(self, state):
-        action, prob = self.mcts(state)
-        print('CPU win belief: {}%'.format(int(100*prob)))
+        action, probs = self.mcts(state)
+        if self.verbose:
+            print('CPU({}) win belief: {}% ({})'.format(self.name, int(100*max(probs)), np.round(100*probs,0)))
         return action
 
 class HumanAgent:
@@ -155,25 +91,31 @@ class HumanAgent:
             action = input("player {}'s next move? ({}): ".format(int(state[-1]), ''.join(actions))).upper()
         return Mancala.letter_to_bin(action)
 
-def play(userGoesFirst=False):
-    env = Mancala(render_mode='human', render_unicode=True)
-    user = HumanAgent()
-    cpu = MCTSAgent(nsamples=3000)
+def get_players(player1, player2, verbose=False, nsamples=3000):
+    if player1 == 'human':
+        player1 = HumanAgent()
+    else:
+        player1 = MonteCarloRolloutAgent(name='P1', nsamples=nsamples, verbose=verbose)
+    if player2 == 'human':
+        player2 = HumanAgent()
+    else:
+        player2 = MonteCarloRolloutAgent(name='P2', nsamples=nsamples, verbose=verbose)
+    return [player1, player2]
 
+def play(player1, player2):
+    env = Mancala(render_mode='human', render_unicode=True)
     state, _ = env.reset()
     terminated = False
 
+    players = get_players(player1, player2, verbose=True, nsamples=3000)
     while not terminated:
-        if (userGoesFirst and state[-1] == 1) or (not userGoesFirst and state[-1] == 2):
-            action = user.get_action(state)
-        else:
-            action = cpu.get_action(state)
+        action = players[int(state[-1])-1].get_action(state)
         state, _, terminated, _, _ = env.step(action)
 
 if __name__ == "__main__":
-    import sys
-    try:
-        userGoesFirst = int(sys.argv[1]) == 1
-    except:
-        userGoesFirst = True
-    play(userGoesFirst)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('player1', choices=['human', 'cpu'])
+    parser.add_argument('player2', choices=['human', 'cpu'])
+    args = parser.parse_args()
+    play(args.player1, args.player2)
