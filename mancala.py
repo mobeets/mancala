43ad730bdb4939288b0494acd4867617a158e3d2
Jay H <mobeets@gmail.com>
2023-10-07 14:24:06 -0400

    init

---
+++ mancala.py
@@ -0,0 +1,185 @@
+import numpy as np
+
+class Mancala:
+    metadata = {"render_modes": ["human"]}
+
+    def __init__(self, render_mode=None, render_unicode=True):
+        self.state = []
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_unicode = render_unicode
+        self.render_mode = render_mode
+        self.observation_space = None # todo
+        self.action_space = None # todo
+
+    def initial_game_state(self):
+        # encoding:
+        #   0  1  2   3  4  5  6  7  8  9  10  11  12 13 14
+        #   B1 B2 B3 B4 B5 B6 M1 B7 B8 B9 B10 B11 B12 M2 P
+        # where:
+        #   Bi = bin i
+        #   Mj = mancala for player j
+        #   P = player id of whose turn it is
+        state = np.zeros(15) # initialize to all zeros
+        state[0:6] = 4  # player 1's bins all start with 4 stones
+        state[7:13] = 4 # player 2's bins all start with 4 stones
+        state[-1] = 1 # player 1 goes first
+        return state.tolist()
+
+    def reset(self, seed=None, options=None):
+        self.state = self.initial_game_state()
+        if self.render_mode == "human":
+            self.render(unicode=self.render_unicode)
+        return self.state, {}
+
+    @staticmethod
+    def is_terminated(state):
+        # game is over when one player's bins are all empty
+        return sum(state[0:6]) == 0 or sum(state[7:13]) == 0
+
+    @staticmethod
+    def next_state(state, action):
+        prev_state = [x for x in state]
+        state = [x for x in state] # make copy
+
+        # confirm game is not over
+        assert not Mancala.is_terminated(state)
+
+        # confirm action is valid given whose turn it is
+        if state[-1] == 1:
+            assert action in list(range(0,6))
+        else:
+            assert action in list(range(7,13))
+        assert state[action] > 0
+        
+        # move stones
+        remaining = state[action]
+        state[action] = 0
+        next_bin = action
+        while remaining > 0:
+            next_bin += 1
+            if next_bin > 13: # wrap around
+                next_bin = 0
+            elif next_bin == 6 and state[-1] == 2:
+                # if player 2's turn, don't place in player 1's mancala
+                next_bin += 1
+            elif next_bin == 13 and state[-1] == 1:
+                # if player 1's turn, don't place in player 2's mancala
+                next_bin = 0
+            state[next_bin] += 1
+            remaining -= 1
+        next_state = [x for x in state]
+
+        # if last stone was placed in empty bin, current player steals other player's adjacent stones
+        if state[next_bin] == 1 and next_bin not in [6,13]:
+            # make sure this was our own empty bin
+            if (state[-1] == 1 and next_bin < 6) or (state[-1] == 2 and next_bin > 6):
+                # steal opponent's stone
+                adjacent_bin = 13-(next_bin+1)
+                stolen_stones = state[adjacent_bin]
+                state[adjacent_bin] = 0
+                mancala = 6 if state[-1] == 1 else 13
+                state[mancala] += stolen_stones
+
+                # we also get our stone
+                state[mancala] += 1
+                state[next_bin] = 0
+
+        # if last stone was not placed in mancala, it's now the other player's turn
+        if next_bin not in [6,13]:
+            state[-1] = (1 if state[-1] == 2 else 2)
+        
+        assert sum(state[:-1]) == 4*12
+        return [int(x) for x in state]
+
+    def step(self, action):
+        self.state = Mancala.next_state(self.state, action)
+        terminated = Mancala.is_terminated(self.state)
+        reward = 0 # todo
+        if self.render_mode == "human":
+            self.render(action=action, unicode=self.render_unicode)
+        return self.state, reward, terminated, False, {}
+
+    @staticmethod
+    def get_current_scores(state):
+        return sum(state[0:7]), sum(state[7:14])
+
+    @staticmethod
+    def get_winner(state):
+        # returns winning player
+        s1, s2 = Mancala.get_current_scores(state)
+        return 1 if s1 > s2 else (2 if s2 > s1 else -1)
+
+    @staticmethod
+    def get_valid_actions(state):
+        # action encoding is the index of a bin (in state vector) to select
+
+        # figure out what actions are available to that player
+        if state[-1] == 1: # player 1 turn
+            valid_actions = list(range(0,6))
+        else:
+            valid_actions = list(range(7,13))
+        assert len(valid_actions) == 6
+
+        # can only choose non-empty bins
+        valid_actions = [action for action in valid_actions if state[action] > 0]
+        return valid_actions
+
+    @staticmethod
+    def bin_to_letter(action):
+        return 'ABCDEF LKJIHG'[action]
+
+    @staticmethod
+    def letter_to_bin(letter):
+        return 'ABCDEF LKJIHG'.index(letter)
+
+    @staticmethod
+    def count_to_char(count, unicode=False):
+        if not unicode:
+            return str(int(count))
+        if count == 0:
+            return ' '
+        elif count == 1:
+            return '.'
+        elif count == 2:
+            return ':'
+        elif count == 3:
+            return '\u2E2A'
+        elif count == 4:
+            return '\u2E2C'
+        elif count == 5:
+            return '\u2059'
+        elif count == 6:
+            return '\u283F'
+        elif count == 7:
+            return '\u28BF'
+        elif count == 8:
+            return '\u28FF'
+        else:
+            return str(int(count))
+
+    def render(self, state=None, action=None, unicode=False):
+        #    B12 B11 B10 B9 B8 B7
+        # M2                       M1
+        #    B1   B2  B3 B4 B5 B6
+
+        state = self.state if state is None else state
+        if action is not None:
+            print('player chose {}\n'.format(Mancala.bin_to_letter(action)))
+
+        # print game board
+        line0 = ' '*5 + ' '.join([Mancala.bin_to_letter(i) for i in range(12,6,-1)])
+        line1 = ' '*5 + ' '.join([Mancala.count_to_char(self.state[i], unicode) for i in range(12,6,-1)])
+        line2 = str(int(self.state[13])) + ' '*20 + str(int(self.state[6]))
+        line3 = ' '*5 + ' '.join([Mancala.count_to_char(self.state[i], unicode) for i in range(0,6)])
+        line4 = ' '*5 + ' '.join([Mancala.bin_to_letter(i) for i in range(0,6)])
+        out = ' '*10 + '←\n'
+        out += '\n'.join([line0, line1, line2, line3, line4])
+        out += '\n' + ' '*10 + '→\n'
+        out += '\n\nplayer {} to move'.format(int(self.state[-1]))
+
+        # print winner
+        if Mancala.is_terminated(state):
+            out += "\nplayer {} wins: {}".format(Mancala.get_winner(state), Mancala.get_current_scores(state))
+
+        print(out)
+        return out
---
+++ play.py
@@ -0,0 +1,174 @@
+from mancala import Mancala
+import numpy as np
+
+def estimate_value(data, player_num):
+    # data is list of (state, action, next_state)
+    # we only use current state to figure out whose turn it is
+    # and also check if game is over, in which case we need no heuristic
+    state = data[-1][2]
+    if Mancala.is_terminated(state):
+        # no heuristic needed: value is inf if we won, -inf if we lost
+        return np.inf if Mancala.get_winner(state) == player_num else -np.inf
+    else:
+        start_state = data[0][0]
+        s1_init, s2_init = Mancala.get_current_scores(start_state)
+        s1_final, s2_final = Mancala.get_current_scores(state)
+        # value is equal to our improvement in score differential
+        score_delta = (s1_final - s2_final) - (s1_init - s2_init)
+        return score_delta if start_state[-1] == 1 else -score_delta
+
+def get_auto_action(state):
+    # if any action leads to an extra move, we make them here
+    actions = Mancala.get_valid_actions(state)
+    possible_auto_actions = []
+    for action in actions:
+        next_state = Mancala.next_state(state, action)
+        if next_state[-1] == state[-1]:
+            # taking this action would not change whose turn it is
+            possible_auto_actions.append(action)
+    if possible_auto_actions:
+        # take the auto action closest to our mancala first
+        # to ensure we can take the others on our next turn
+        return max(possible_auto_actions)
+    else:
+        return None
+
+def rollout_best_move(state, action, depth, nsamples):
+    data = []
+    for d in range(depth+1):
+        if d > 0:
+            action = best_move(state, depth-d, nsamples)        
+        next_state = Mancala.next_state(state, action)
+        data.append((state, action, next_state))
+        state = next_state
+    return data
+
+def best_move(state, depth, nsamples=1):
+    # first check for any automatic moves, and add them here
+    chosen_action = get_auto_action(state)
+    if chosen_action:
+        return chosen_action
+
+    # now check for best next move
+    actions = Mancala.get_valid_actions(state)
+    values = []
+    for action in actions:
+        cvalues = []
+        for _ in range(nsamples):
+            data = rollout_best_move(state, action, depth, nsamples)
+            value = estimate_value(data, state[-1])
+            cvalues.append(value)
+        values.append(np.mean(cvalues))
+    return actions[np.argmax(values)]
+
+class HeuristicAgent:
+    def __init__(self, depth=2):
+        self.depth = depth
+
+    def get_action(self, state):
+        return best_move(state, self.depth)
+
+# def sim(opening_actions, depth, check_auto_first=False, do_render=False):
+#     state = initial_game_state()
+#     if do_render:
+#         render(state)
+#         input()
+#     i = 0
+#     while not is_terminated(state):
+#         if check_auto_first and get_auto_action(state):
+#             action = get_auto_action(state)
+#         elif opening_actions:
+#             action = opening_actions.pop()
+#             if action not in get_valid_actions(state):
+#                 return None, None
+#         else:
+#             action = best_move(state, depth)
+#         state = step(state, action)
+#         if do_render:
+#             render(state, action)
+#             input()
+#         i += 1
+#     if do_render:
+#         print()
+#         print("player {} wins: {}".format(get_winner(state), get_current_scores(state)))
+#     return get_current_scores(state), i
+
+# def analyze_best_move(depth=2):
+#     for first_action in range(6):
+#         scores, nsteps = sim([first_action], depth)
+#         if scores is not None:
+#             score_diff = -np.diff(scores)[0]
+#         else:
+#             score_diff = 'DNE'
+#         print(first_action, score_diff, nsteps)
+
+# MCTS
+
+class MCTSAgent:
+    def __init__(self, nsamples=100):
+        self.nsamples = nsamples
+
+    def get_rollout_policy_action(self, state):
+        pass
+
+    def rollout(self, state, action):
+        terminated = True
+        i = 0
+        data = [state]
+        while not terminated:
+            if i > 0:
+                action = self.get_rollout_policy_action(state)
+            state = Mancala.next_state(state, action)
+            data.append(state)
+            i += 1
+        return data
+
+    def get_return(self, data, player_num):
+        final_state = data[-1]
+        assert Mancala.is_terminated(final_state)
+        return 1 if Mancala.get_winner(final_state) == player_num else 0
+
+    def mcts(self, state, nsamples):
+        actions = Mancala.get_valid_actions(state)
+        mean_payouts = []
+        for action in actions:
+            payouts = []
+            for _ in range(nsamples):
+                data = rollout(state, action, depth, nsamples)
+                payout = self.get_return(data, state[-1])
+                payouts.append(payout)
+            mean_payouts.append(np.mean(payouts))
+        return actions[np.argmax(mean_payouts)]
+
+    def get_action(self, state):
+        return self.mcts(state, self.nsamples)
+
+class HumanAgent:
+    def get_action(self, state):
+        actions = [Mancala.bin_to_letter(i) for i in Mancala.get_valid_actions(state)]
+        action = -1
+        while action not in actions:
+            action = input("player {}'s next move? ({}): ".format(int(state[-1]), ''.join(actions)))
+        return Mancala.letter_to_bin(action)
+
+def play(userGoesFirst=False):
+    env = Mancala(render_mode='human', render_unicode=True)
+    user = HumanAgent()
+    cpu = HeuristicAgent()
+    state, _ = env.reset()
+    terminated = False
+
+    while not terminated:
+        if (userGoesFirst and state[-1] == 1) or (not userGoesFirst and state[-1] == 2):
+            action = user.get_action(state)
+        else:
+            action = cpu.get_action(state)
+        state, _, terminated, _, _ = env.step(action)
+
+if __name__ == "__main__":
+    import sys
+    try:
+        userGoesFirst = int(sys.argv[1])
+    except:
+        userGoesFirst = True
+    play(userGoesFirst)
